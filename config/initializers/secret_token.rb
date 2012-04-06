02a912e9ac3b970a37ec2df2fa0578fc6ed5e0b7
foxgaocn <foxgaocn@gmail.com>
2012-04-06 20:49:36 +1000

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Couponfb::Application.config.secret_token = 'b9241179d83b4af66972a4d5f29709000f7379a2d16bef79a825072d4356ad60b2f60bfd792883aec9a0b600648a2f50d9ebd3239b8482105a602817544940f5'
