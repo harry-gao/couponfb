b451f5ed2b8231ac9ca4ee8b63b0290c54e6768d
foxgaocn <foxgaocn@gmail.com>
2012-04-10 21:56:34 +1000

    FB integration part 2

--- app/assets/javascripts/fb_integration.js
+++ app/assets/javascripts/fb_integration.js
@@ -1,12 +1,20 @@
 $(function() {
-    var $dialog = $("#fb_login_dialog").dialog({
+    $login_dialog = $("#fb_login_dialog").dialog({
 			autoOpen: false,
 			title: 'Welcome to couponfb'
 		});
 
-	$('.apply_button').click(function() {
-		$dialog.dialog('open');
-		// prevent the default action, e.g., following a link
+	$('.apply_button').click(function(eventObject) {
+        $current_apply_item = eventObject.currentTarget;
+
+        if(!g_fb_connected)
+        {
+		    $login_dialog.dialog('open');
+        }
+        else
+        {
+            postToFeed();
+        }
 		return false;
 	});
 
@@ -14,6 +22,11 @@ $(function() {
        FB.logout();
     });
 });
+
+var g_fb_connected = false;
+var $login_dialog;
+var $current_apply_item;
+
 
 (function(d){
     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
@@ -40,15 +53,45 @@ window.fbAsyncInit = function() {
 function connectionStatusChanged(response)
 {
   if(response.status=="connected"){
+      g_fb_connected = true;
       $("#logged_in_panel").css('display', 'block');
       FB.api('/me', function(response) {
           $('#fb_user_name').html(response.name);
         });
       $("#not_logged_in_panel").css('display', 'none');
+
+      if($login_dialog.dialog( "isOpen" )){
+          $login_dialog.dialog('close');
+          postToFeed();
+      }
   }
   else{
+      g_fb_connected = false;
       $("#logged_in_panel").css('display', 'none');
       $("#not_logged_in_panel").css('display', 'block');
   }
+}
+
+function postToFeed()
+{
+    var obj = {
+      method: 'feed',
+      link: $current_apply_item.dataset.link,
+      picture: $current_apply_item.dataset.pic,
+      name: $current_apply_item.dataset.title,
+      caption: $current_apply_item.dataset.ad
+    };
+
+    function callback(response) {
+        if(response && response['post_id']){
+            alert('thank you, code is 1234');
+        }
+        else
+        {
+            alert('sorry you have to share the information to get the code');
+        }
+    }
+
+    FB.ui(obj, callback);
 }
 
